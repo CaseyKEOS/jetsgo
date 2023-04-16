acb816c9e227ffda5d5d8bf70149ba1af7501f5b
CaseyKEOS <caseykentemmanuel@gmail.com>
2023-04-16 20:45:31 +0800

    https://github.com/CaseyKEOS/jetsgo.git

--- index.php
+++ index.php
@@ -13,81 +13,73 @@
     <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/assets/php/"; include($IPATH."nav.php"); ?>
 
   <div class="card">
-    <div class="body">
-      <div class="carousel 300-px-wide">
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
-          <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          </div>
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src="img/phbeach.jpg" class="d-block w-100" alt="...">
-              <div class="carousel-caption d-none d-md-block">
-                <h5>Boracay, Aklan</h5>
-                <p>Some representative placeholder content for the first slide.</p>
+    <section>
+        <div class="body">
+          <div class="carousel 300-px-wide">
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
               </div>
-            </div>
-            <div class="carousel-item">
-              <img src="img/coron.jpg" class="d-block w-100" alt="...">
-              <div class="carousel-caption d-none d-md-block">
-                <h5>Coron, Palawan</h5>
-                <p>Some representative placeholder content for the second slide.</p>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="img/phbeach.jpg" class="d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Boracay, Aklan</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="img/coron.jpg" class="d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Coron, Palawan</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="img/subic.jpg" class="d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Subic, Zambales</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div class="carousel-item">
-              <img src="img/subic.jpg" class="d-block w-100" alt="...">
-              <div class="carousel-caption d-none d-md-block">
-                <h5>Subic, Zambales</h5>
-                <p>Some representative placeholder content for the third slide.</p>
-              </div>
-            </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
-      </div>
-    </div>
-  <section class="contact_section layout_padding">
-    <div class="container ">
-      <h2 class="">
-        Ask Anything
-      </h2>
-    </div>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 col-lg-6 mx-auto">
-          <form action="" method="POST">
-            <div>
-              <input type="text" name="name" placeholder="Name" />
-            </div>
-            <div>
-              <input type="email" name="email" placeholder="Email" />
-            </div>
-            <!-- <div>
-              <input type="text" placeholder="Phone Number" />
-            </div> -->
-            <div>
-              <input type="text" class="message-box" placeholder="Message" />
-            </div>
-            <div class="d-flex justify-content-center">
-              <button type="submit">
-                SEND
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
               </button>
             </div>
-          </form>
+          </div>
         </div>
-
-      </div>
-    </div>
-  </section>
+    </section>
+   <section>
+        <div class="container mb-3">
+          <div class="row">
+            <div class="col-md-8 col-lg-6 mx-auto">
+              <form action="send.php" method="POST">
+                <div class="mb-3">
+                  <input type="email" name="email" class="form-control" placeholder="sample@gmail.com" value="" required/>
+                </div>
+                <div class="mb-3">
+                  <input type="text" name="subject" class="form-control" placeholder="Subject" value="" />
+                </div>
+                
+                <div class="mb-3">
+                  <textarea type="text" name="message" class="message-box form-control" placeholder="Message" value=""></textarea>
+                </div>
+                <div class="d-flex justify-content-center mb-3">
+                  <button type="submit" name="send" class="btn btn-success">SEND</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+   </section>
 
 <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/assets/php/"; include($IPATH."footer.php"); ?>
 
